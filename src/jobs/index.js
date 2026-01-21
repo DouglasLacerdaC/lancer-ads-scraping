9161f10d6af778dad2960e310947287333633bfe
Douglas Lacerda <douglaslacerda544@gmail.com>
2026-01-21 16:46:23 -0300

    :sparkles: feat: atualizar novos anuncios

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -7,29 +7,33 @@ import cron from "node-cron"
 import { webmotorsService } from "../services/webmotors.js";
 
 export async function updateNewAds() {
-  const currentDataJson = await getJSON()
+  try {
+    const currentDataJson = await getJSON()
 
-  const olxAds = await olxService.getAds()
-  const webmotorsAds = await webmotorsService.getAds()
+    const olxAds = await olxService.getAds()
+    const webmotorsAds = await webmotorsService.getAds()
 
-  const newData = [...olxAds, ...webmotorsAds].filter(ad => {
-    const isValid = currentDataJson.find(data => data.link == ad.link)
+    const newData = [...olxAds, ...webmotorsAds].filter(ad => {
+      const isValid = currentDataJson.find(data => data.link == ad.link)
 
-    if (!isValid) return ad
-  })
+      if (!isValid) return ad
+    })
 
-  for (const newAd of newData) {
-    try {
-      await discordService.sendAD(newAd);
-      await delay(2000)
-    } catch (error) {
-      console.warn("❌ Não foi possível enviar o anúncio: ", error);
+    for (const newAd of newData) {
+      try {
+        await discordService.sendAD(newAd);
+        await delay(2000)
+      } catch (error) {
+        console.warn("❌ Não foi possível enviar o anúncio: ", error);
+      }
     }
+
+    console.warn(`\n🚀 [${newData.length}] novos anúncios encontrados\n`)
+
+    setJSON([...currentDataJson, ...newData])
+  } catch (error) {
+    console.log(`\n❌ [Erro] Não foi possível finalizar a busca dos anúncios. Erro: ${error}\n`)
   }
-
-  console.warn(`\n🚀 [${newData.length}] novos anúncios encontrados\n`)
-
-  setJSON([...currentDataJson, ...newData])
 }
 
 export const registerJobs = () => {
